46afa8131a2a7631f8aab2c9d5763d9ed7740889
Darren Wong <email@darrenwong.net>
2021-12-10 14:48:22 +1100

    3.3 commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const express = require('express');
 const app = express();
 
@@ -33,6 +34,19 @@ app.get('/info', (request, response) => {
 
 app.get('/api/persons', (request, response) => {
     response.json(persons);
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find(person => person.id === id);
+
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).end();
+    }
+
+    
 })
 
 const PORT = 3001;
