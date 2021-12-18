e9471173a8926ac280a5beb8417a35bf811d93ac
Darren Wong <email@darrenwong.net>
2021-12-18 23:13:42 +1100

    3.22 commit

--- index.js
+++ index.js
@@ -1,138 +1,139 @@
-require('dotenv').config()
-const express = require('express');
-var morgan = require('morgan');
-const cors = require('cors');
-const app = express();
-const Person = require('./models/person')
+require("dotenv").config()
+const express = require("express")
+var morgan = require("morgan")
+const cors = require("cors")
+const app = express()
+const Person = require("./models/person")
 
-app.use(cors());
-app.use(express.static('build'));
-app.use(express.json());
+app.use(cors())
+app.use(express.static("build"))
+app.use(express.json())
 
 // Define error handling function
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-  
-    next(error)
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" })
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
 }
-  
+
 app.use(errorHandler)
 
-morgan.token('post_contents', (request, response) => {
-    if (request.body.name) {
-        return JSON.stringify(request.body)
-    }});
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post_contents'));
+morgan.token("post_contents", (request) => {
+  if (request.body.name) {
+    return JSON.stringify(request.body)
+  }})
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :post_contents"))
 
-app.get('/info', (request, response, next) => {
+app.get("/info", (request, response, next) => {
 
-    Person
-        .find({})
-        .then(result => {
-            response.send(`
+  Person
+    .find({})
+    .then(result => {
+      response.send(`
             <p>Phonebook has info for ${result.length} people</p>
             <p>${new Date()}</p>
             `)
-        })
-        .catch(error => next(error))
+    })
+    .catch(error => next(error))
 
 })
 
-app.get('/api/persons', (request, response, next) => {
+app.get("/api/persons", (request, response, next) => {
 
-    Person
-        .find({})
-        .then(person => response.json(person))
-        .catch(error => next(error))
-        
-})
-
-app.get('/api/persons/:id', (request, response, next) => {
-
-    Person
-        .findById(request.params.id)
-        .then(person => response.json(person))
-        .catch(error => next(error))
+  Person
+    .find({})
+    .then(person => response.json(person))
+    .catch(error => next(error))
 
 })
 
-app.delete('/api/persons/:id', (request, response, next) => {
+app.get("/api/persons/:id", (request, response, next) => {
 
-    Person
-        .findByIdAndRemove(request.params.id)
-        .then(result => response.status(204).end())
-        .catch(error => next(error))
+  Person
+    .findById(request.params.id)
+    .then(person => response.json(person))
+    .catch(error => next(error))
 
 })
 
-app.post('/api/persons', (request, response, next) => {
-    const body = request.body;
+app.delete("/api/persons/:id", (request, response, next) => {
 
-    // If body is empty return error response
-    if (!body.name || !body.number) {
+  Person
+    .findByIdAndRemove(request.params.id)
+    .then(() => response.status(204).end())
+    .catch(error => next(error))
 
-        return response.status(400).json({ 
-            error: 'content missing' 
+})
+
+app.post("/api/persons", (request, response, next) => {
+  const body = request.body
+
+  // If body is empty return error response
+  if (!body.name || !body.number) {
+
+    return response.status(400).json({
+      error: "content missing"
+    })
+
+  }
+
+  Person
+  // First, check if there is another entry with the same name - case insensitive
+    .find({})
+    .then(result => result.map(per => per.name))
+    .then(result => result.filter(el => el.toLowerCase() === body.name.toLowerCase()))
+  // If there is; update it. If not, add a new entry
+    .then(result => {
+
+      if (result.length > 0) {
+
+        const person = {
+          number: body.number
+        }
+
+        Person
+          .findOneAndUpdate(
+            { name: result[0] },
+            person,
+            // Adding a blocker so that a unique name cannot be updated here
+            //  feels counter-intuitive to this part; hopefully am interpreting
+            //  the exercise correctly.
+            { runValidators: true, context: "query", new: true }
+          )
+          .then(updatedPerson => {
+            return response.json(updatedPerson)
           })
+          .catch(error => next(error))
 
-    }
+      } else {
 
-    Person
-        // First, check if there is another entry with the same name - case insensitive
-        .find({})
-        .then(result => result.map(per => per.name))
-        .then(result => result.filter(el => el.toLowerCase() === body.name.toLowerCase()))
-        // If there is; update it. If not, add a new entry
-        .then(result => {
-
-            if (result.length > 0) {
-
-                const person = {
-                    number: body.number
-                }
-
-                Person
-                    .findOneAndUpdate(
-                        { name: result[0] },
-                        person,
-                        // Adding a blocker so that a unique name cannot be updated here
-                        //  feels counter-intuitive to this part; hopefully am interpreting
-                        //  the exercise correctly.
-                        { runValidators: true, context: 'query', new: true }
-                    )
-                    .then(updatedPerson => {
-                        return response.json(updatedPerson)
-                    })
-                    .catch(error => next(error))
-
-            } else {
-
-                const person = new Person({
-                    name:   body.name,
-                    number: body.number,
-                    date:   new Date()
-                })
-
-                person
-                    .save()
-                    .then(savedPerson => {
-                        return response.json(savedPerson)
-                    })
-                    .catch(error => next(error))
-
-            }
-
+        const person = new Person({
+          name:   body.name,
+          number: body.number,
+          date:   new Date()
         })
+
+        person
+          .save()
+          .then(savedPerson => {
+            return response.json(savedPerson)
+          })
+          .catch(error => next(error))
+
+      }
+
+    })
 
 })
 
-const PORT = process.env.PORT;
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- models/person.js
+++ models/person.js
@@ -1,32 +1,33 @@
-const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator');
+const mongoose = require("mongoose")
+const uniqueValidator = require("mongoose-unique-validator")
 
+// eslint-disable-next-line no-undef
 const url = process.env.MONGODB_URI
 
-console.log('Connecting to', url)
+console.log("Connecting to", url)
 
 mongoose.connect(url)
-  .then(result => {
-    console.log('Connected to MongoDB')
+  .then(() => {
+    console.log("Connected to MongoDB")
   })
   .catch((error) => {
-    console.log('Error connecting to MongoDB:', error.message)
+    console.log("Error connecting to MongoDB:", error.message)
   })
 
 
 const personSchema = new mongoose.Schema({
-    name: { type: String, required: true, minLength: 3, unique: true, uniqueCaseInsensitive: true },
-    number: { type: String, required: true, minLength: 8 }
+  name: { type: String, required: true, minLength: 3, unique: true, uniqueCaseInsensitive: true },
+  number: { type: String, required: true, minLength: 8 }
 })
 
-personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 personSchema.plugin(uniqueValidator)
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model("Person", personSchema)
