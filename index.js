28ef2d583a38d057de7a7c7abeb533c4e1b997f3
Darren Wong <email@darrenwong.net>
2021-12-18 18:33:11 +1100

    3.15 commit, working locally

--- index.js
+++ index.js
@@ -68,10 +68,13 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    persons = persons.filter(person => person.id !== id);
 
-    response.status(204).end();
+    Person
+        .findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end();
+        })
+
 })
 
 app.post('/api/persons', (request, response) => {
